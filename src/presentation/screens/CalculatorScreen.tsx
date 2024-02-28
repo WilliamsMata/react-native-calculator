db931e42d183301cb9e39049fa6b4f9a3ef9ab4d
williamsmata <williams.rm99@gmail.com>
2024-02-28 10:50:28 -0400

    Agregada funcionalidad de calculadora y mejoras en la interfaz de usuario

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -2,30 +2,39 @@ import {Text, View} from 'react-native';
 import React from 'react';
 import {colors, globalStyles} from '../../config/theme/app-theme';
 import {CalculatorButton} from '../components/CalculatorButton';
+import {useCalculator} from '../hooks/useCalculator';
 
 export function CalculatorScreen() {
+  const {number, buildNumber, toggleNumberSign, clear, deleteLast} =
+    useCalculator();
+
   return (
     <View style={globalStyles.calculatorContainer}>
       <View style={globalStyles.resultContainer}>
-        <Text style={globalStyles.mainResult}>1500</Text>
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={1}
+          style={globalStyles.mainResult}>
+          {number}
+        </Text>
         <Text style={globalStyles.subResult}>15</Text>
       </View>
 
       <View style={globalStyles.row}>
         <CalculatorButton
-          onPress={() => console.log('C')}
+          onPress={() => clear()}
           blackText
           label="C"
           color={colors.lightGray}
         />
         <CalculatorButton
-          onPress={() => console.log('+/-')}
+          onPress={() => toggleNumberSign()}
           blackText
           label="+/-"
           color={colors.lightGray}
         />
         <CalculatorButton
-          onPress={() => console.log('del')}
+          onPress={() => deleteLast()}
           blackText
           label="del"
           color={colors.lightGray}
@@ -38,9 +47,9 @@ export function CalculatorScreen() {
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton onPress={() => console.log('7')} label="7" />
-        <CalculatorButton onPress={() => console.log('8')} label="8" />
-        <CalculatorButton onPress={() => console.log('9')} label="9" />
+        <CalculatorButton onPress={() => buildNumber('7')} label="7" />
+        <CalculatorButton onPress={() => buildNumber('8')} label="8" />
+        <CalculatorButton onPress={() => buildNumber('9')} label="9" />
         <CalculatorButton
           onPress={() => console.log('×')}
           label="×"
@@ -49,9 +58,9 @@ export function CalculatorScreen() {
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton onPress={() => console.log('4')} label="4" />
-        <CalculatorButton onPress={() => console.log('5')} label="5" />
-        <CalculatorButton onPress={() => console.log('6')} label="6" />
+        <CalculatorButton onPress={() => buildNumber('4')} label="4" />
+        <CalculatorButton onPress={() => buildNumber('5')} label="5" />
+        <CalculatorButton onPress={() => buildNumber('6')} label="6" />
         <CalculatorButton
           onPress={() => console.log('-')}
           label="-"
@@ -60,9 +69,9 @@ export function CalculatorScreen() {
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton onPress={() => console.log('1')} label="1" />
-        <CalculatorButton onPress={() => console.log('2')} label="2" />
-        <CalculatorButton onPress={() => console.log('3')} label="3" />
+        <CalculatorButton onPress={() => buildNumber('1')} label="1" />
+        <CalculatorButton onPress={() => buildNumber('2')} label="2" />
+        <CalculatorButton onPress={() => buildNumber('3')} label="3" />
         <CalculatorButton
           onPress={() => console.log('+')}
           label="+"
@@ -72,11 +81,11 @@ export function CalculatorScreen() {
 
       <View style={globalStyles.row}>
         <CalculatorButton
-          onPress={() => console.log('0')}
+          onPress={() => buildNumber('0')}
           label="0"
           dobleSize
         />
-        <CalculatorButton onPress={() => console.log('.')} label="." />
+        <CalculatorButton onPress={() => buildNumber('.')} label="." />
         <CalculatorButton
           onPress={() => console.log('=')}
           label="="
